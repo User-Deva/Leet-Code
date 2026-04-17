c8641d952db318c14d280159d4bff758bfed3ba9
Devang Patidar <49941619+User-Deva@users.noreply.github.com>
2026-04-16 21:00:08 -0400

    Day 3, 4

---
+++ Search insert/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        i = 0
+        j = n-1
+        while i <= j:
+            mid = (i+j)//2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                i = mid + 1
+            else:
+                j = mid - 1
+        return i
