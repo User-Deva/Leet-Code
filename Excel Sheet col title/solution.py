8074029f1abf945d8d2d1991fecc7af8e04d5a61
Devang Patidar <49941619+User-Deva@users.noreply.github.com>
2026-05-27 11:32:51 -0400

    new day

---
+++ Excel Sheet col title/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        r=''
+        while columnNumber>0:
+            columnNumber-=1
+            r=(chr(ord('A')+columnNumber%26))+r
+            columnNumber//=26
+        return r
+        
